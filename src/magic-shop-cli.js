289756808d42705b3be77cf70304b0200f71d643
Jason Kleinberg <ustice@gmail.com>
2017-05-15 15:09:37 -0400

    Adds simple cli tool

---
+++ src/magic-shop-cli.js
@@ -0,0 +1,46 @@
+'use strict';
+
+const path = require('path');
+const magicItems = require(path.join(__dirname, '/data/magic-items.json'));
+
+const threshold = {
+  0: .5,
+  1: .3,
+  2: .2,
+  3: .1,
+  4: 0,
+};
+
+const priceFunction = {
+  0: () => (rollDice(1,6) + 1) * 10,
+  1: () => rollDice(1,6) * 100,
+  2: () => rollDice(2, 10) * 1000,
+  3: () => (rollDice(1, 4) + 1) * 10000,
+  4: () => rollDice(2, 6) * 25000,
+};
+
+const roll = sides => Math.floor(Math.random() * sides) + 1;
+
+const rollDice = (number, sides) => {
+  return new Array(number)
+    .fill(null)
+    .map(() => roll(sides))
+    .reduce((accumulator, value) => accumulator + value, 0)
+  ;
+};
+
+const calculatePrice = item => priceFunction[item.rarity]() * (item.type === 'Wondrous Item' ? 1.5 : 1);
+
+const inStock = magicItems
+  .filter(item => {
+    const die = Math.random();
+
+    return die <= threshold[item.rarity];
+  })
+  .map(item => {
+    item.price = calculatePrice(item);
+    return item;
+  })
+;
+
+console.log(inStock);
